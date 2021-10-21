7f2391d47f94ce3bd2f1b613f0d4ebfdadc60d21
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-10-21 15:55:31 -0500

    Funcionando full

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-  const num1 = req.query.num1;
-  const num2 = req.query.num2;
+  const num1 = parseInt(req.query.num1);
+  const num2 = parseInt(req.query.num2);
+  const resultado = num1 + num2;
+  res.render("template", { valor: resultado });
 });
+
+app.listen(3000);
