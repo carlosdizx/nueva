ab16f3adbbb94c156a83297dd6c79287c5a51365
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-10-21 17:15:07 -0500

    Funcionando dull las operaciones

--- index.js
+++ index.js
@@ -7,8 +7,33 @@ app.set("view engine", "hbs");
 app.get("/", (req, res) => {
   const num1 = parseInt(req.query.num1);
   const num2 = parseInt(req.query.num2);
-  const resultado = num1 + num2;
-  res.render("template", { valor: resultado });
+  const datos = Object.values(req.query);
+  let total = 0;
+  let subtotal = 0;
+  let cantidad = 0;
+  let descuento = "No aplica";
+  for (let i = 0; i < datos.length; i++) {
+    if (i % 2 == 0) {
+      const precio = datos[i];
+      const cantidadActual = datos[i + 1];
+      if (cantidadActual) {
+        cantidad += parseInt(cantidadActual);
+        if (cantidadActual >= 10) {
+          total += precio * cantidadActual * 0.9;
+          descuento = "Aplicado";
+        } else {
+          total += precio * cantidadActual;
+        }
+        subtotal += precio * cantidadActual;
+      }
+    }
+  }
+  res.render("template", {
+    cantidad: cantidad,
+    subtotal: subtotal,
+    descuento: descuento,
+    total: total,
+  });
 });
 
 app.listen(3000);
